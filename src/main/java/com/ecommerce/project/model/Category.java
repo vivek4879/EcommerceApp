154aa341ab3f703e344986923c257dff6cc294a6
Vivek Aher <vivek4879gmail.com>
2025-08-06 16:34:14 -0400

    Added lombok dependency and updated Category class accordingly to remove getters setters and constructors

--- src/main/java/com/ecommerce/project/model/Category.java
+++ src/main/java/com/ecommerce/project/model/Category.java
@@ -4,10 +4,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity(name = "categories")
 //JPA requires every entity class to have a public or protected no args constructor
 //create it yourself of use Lombok to create it for you
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Category {
     @Id // marks the filed as the primary key
     @GeneratedValue(strategy = GenerationType.IDENTITY) //tells JPA that let DB handle auto incrementing this column
@@ -15,26 +21,26 @@ public class Category {
     //AUTO = JPA picks best strategy based on DB dialect. other strategies available
     Long CategoryId;
     String CategoryName;
+//Removed getters, setters, constructors as Lombok the java library will do that for us
+//    public Category() {}
+//    public Category(Long CategoryId, String CategoryName) {
+//        this.CategoryId = CategoryId;
+//        this.CategoryName = CategoryName;
+//    }
 
-    public Category() {}
-    public Category(Long CategoryId, String CategoryName) {
-        this.CategoryId = CategoryId;
-        this.CategoryName = CategoryName;
-    }
-
-    public Long getCategoryId() {
-        return CategoryId;
-    }
-    public void setCategoryId(Long categoryId) {
-        this.CategoryId = categoryId;
-    }
-
-    public String getCategoryName(){
-        return CategoryName;
-    }
-
-    public void setCategoryName(String categoryName){
-        this.CategoryName = categoryName;
-
-    }
+//    public Long getCategoryId() {
+//        return CategoryId;
+//    }
+//    public void setCategoryId(Long categoryId) {
+//        this.CategoryId = categoryId;
+//    }
+//
+//    public String getCategoryName(){
+//        return CategoryName;
+//    }
+//
+//    public void setCategoryName(String categoryName){
+//        this.CategoryName = categoryName;
+//
+//    }
 }
