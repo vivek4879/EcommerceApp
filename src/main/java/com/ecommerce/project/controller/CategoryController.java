27d6b1c104cf93a72720cb41e52003fd03640ee2
Vivek Aher <vivek4879gmail.com>
2025-08-18 16:18:41 -0400

    added defaults for getCategories request parameters and also created a class to hold all app constants in config

--- src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -1,5 +1,6 @@
 package com.ecommerce.project.controller;
 
+import com.ecommerce.project.config.AppConstants;
 import com.ecommerce.project.model.Category;
 import com.ecommerce.project.payload.CategoryDTO;
 import com.ecommerce.project.payload.CategoryResponse;
@@ -38,8 +39,8 @@ public class CategoryController {
 
 
     @GetMapping("/public/categories")
-    public ResponseEntity<CategoryResponse> getCategories(@RequestParam (name = "pageNumber") Integer pageNumber,
-                                                          @RequestParam( name = "pageSize") Integer pageSize) {
+    public ResponseEntity<CategoryResponse> getCategories(@RequestParam (name = "pageNumber",defaultValue = AppConstants.PAGE_NUMBER, required = false) Integer pageNumber,
+                                                          @RequestParam( name = "pageSize",defaultValue = AppConstants.PAGE_SIZE,required = false) Integer pageSize) {
         CategoryResponse categoryResponse =  categoryService.getAllCategories(pageNumber, pageSize);
         return new ResponseEntity<>(categoryResponse, HttpStatus.OK);
     }
